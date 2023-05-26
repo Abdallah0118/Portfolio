3672e5007479291c73fe211ed3bc3dc6e630bc9f
abdallah <abdallah011848@gmail.com>
2023-05-26 12:50:42 +0300

    footer added

--- src/componants/Footer.jsx
+++ src/componants/Footer.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import "./CSS/Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <p class="follow">Follow me.</p>
+      <div class="icons">
+        <a
+          href="https://github.com/Abdallah0118"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i class="bi bi-github"></i>
+        </a>
+        <a
+          href="https://wa.me/+201551533774?text=Hello"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i class="bi bi-whatsapp"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/abdallah-mohamed-347a59250"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i class="bi bi-linkedin"></i>
+        </a>
+      </div>
+      <p class="designed">
+        Developed by <span>Abdallah Mohamed</span> &copy; 2023
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/componants/TechSlider.jsx
+++ src/componants/TechSlider.jsx
@@ -2,7 +2,132 @@ import React from "react";
 import "./CSS/TechSlider.css";
 
 const TechSlider = () => {
-  return <div>TechSlider</div>;
+  return (
+    <section id="techslider">
+      <div
+        className="container row d-flex justify-content-center align-items-center m-auto"
+        id="resposive"
+      >
+        <div className="col-12  col-md-6 d-flex justify-content-center align-items-center">
+          <img
+          className=""
+            id="uiImg"
+            src={require("../assets/prototype.png")}
+            alt="Img Not Found"
+          />
+        </div>
+        <div className="col-12  col-md-6 text-center text-md-start">
+          <h3>BUILDING RESPONSIVE UI</h3>
+          <h4>WITH REACT AND BOOTSTRAP</h4>
+          <p>
+            Responsive Design has been the prime requirement as we build the app
+            considering the “mobile first” approach. So it is more important
+            than ever to make the app responsive for all devices.
+          </p>
+        </div>
+      </div>
+      <div id="slider">
+        <div id="slider-track">
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/bootstrap.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/css-3.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/django.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/git.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/github.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/html-5.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/jquery.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/js.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/mysql.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/node-js.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/postgresql.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/python.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/react.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/redux.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/sass.png")}
+              alt="Img Not Found"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src={require("../assets/Web icons/typescript.png")}
+              alt="Img Not Found"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TechSlider;
