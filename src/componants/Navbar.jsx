d353751b525d13e13c6f8a3a8ea973d175fb0fdd
abdallah <abdallah011848@gmail.com>
2023-05-30 23:24:03 +0300

    Updates to Navbar

--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -5,48 +5,63 @@ import "./CSS/Navbar.css";
 const Navbar = () => {
   return (
     <header>
-      <nav className="navbar navbar-expand-sm">
-        <div className="container">
+      {/* new one  */}
+      <nav class="navbar  navbar-expand-sm">
+        <div class="container">
           <Link className="navbar-brand" to="/" id="logo">
             Abdallah
           </Link>
           <button
-            className="navbar-toggler"
+            class="navbar-toggler shadow-none border-0"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
             aria-label="Toggle navigation"
           >
             <i class="bi bi-list"></i>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul
-              className="navbar-nav ms-auto d-flex justify-content-center align-items-center"
-              id="linksHolder"
-            >
-              <li className="nav-item">
-                <NavLink className="nav-link " to="/" end>
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item ">
-                <NavLink className="nav-link" to="/Projects">
-                  Projects
-                </NavLink>
-              </li>
-              <li className="nav-item ">
-                <NavLink className="nav-link" to="/About">
-                  About
-                </NavLink>
-              </li>
-              <li className="nav-item ">
-                <NavLink className="nav-link" to="/Contact">
-                  Contact
-                </NavLink>
-              </li>
-            </ul>
+          <div
+            class="offcanvas offcanvas-start"
+            tabindex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div class="offcanvas-header">
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="offcanvas-body" id="navbarNav">
+              <ul
+                class="navbar-nav justify-content-end ms-auto"
+                id="linksHolder"
+              >
+                <li className="nav-item">
+                  <NavLink className="nav-link " to="/" end>
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item ">
+                  <NavLink className="nav-link" to="/Projects">
+                    Projects
+                  </NavLink>
+                </li>
+                <li className="nav-item ">
+                  <NavLink className="nav-link" to="/About">
+                    About
+                  </NavLink>
+                </li>
+                <li className="nav-item ">
+                  <NavLink className="nav-link" to="/Contact">
+                    Contact
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
